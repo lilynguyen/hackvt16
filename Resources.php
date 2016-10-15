2a400f1d1d83e73a9fcb96445051d2a48b86d9e1
andydrisc <andy.drisc@gmail.com>
2016-10-15 06:13:31 -0400

    deleted some things
    aka that wack folder

---
+++ Resources.php
@@ -0,0 +1,5 @@
+<html>
+    <title>Schools</title>
+    
+    <p>Below are the public (non-tuition) and non-regional school systems in the state of Vermont. Contact info for their School District </p>
+</html>
